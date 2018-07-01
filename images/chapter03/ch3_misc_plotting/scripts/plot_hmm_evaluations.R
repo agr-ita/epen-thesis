5fcabc4c1512a0de9e162c2255491c96e1ef8e78
Adriana Guevara Rukoz <sk8r_babyita@hotmail.com>
2018-07-01 23:14:51 +0200

    Figure changes

--- images/chapter03/ch3_misc_plotting/scripts/plot_hmm_evaluations.R
+++ images/chapter03/ch3_misc_plotting/scripts/plot_hmm_evaluations.R
@@ -35,23 +35,24 @@ theme_ita <- function (base_size = 15, base_family = "Times") {
 # CREATE DATA ####
 ###############
 
-d.eval <- data.frame(lang=c(rep("CSJ",5)), 
-                     model=c(rep("mono", 4), "tri"),
-                     totgauss=rep(c(1, 3, 6, 15, 15), 1),
-                     WER=c(47.24, 42.28, 40.14, 37.64, 29.94),
-                     PER=c(44.16, 40.92, 39.47, 38.07, 25.33))
-d.eval.melted <- melt(d.eval, id.vars = c("lang", "model", "totgauss"))
+d.eval <- data.frame(corpus=c(rep("CSJ",5), rep("KCSS",5)), 
+                     model=rep(c(rep("mono", 4), "tri"), 2),
+                     totgauss=rep(c(1, 3, 6, 15, 15), 2),
+                     WER=c(c(47.24, 42.28, 40.14, 37.64, 29.94), c(81.39, 77.2, 75.59, 74.03, 65.48)),
+                     PER=c(c(44.16, 40.92, 39.47, 38.07, 25.33), c(56.23, 53.05, 51.66, 50.70, 38.42)))
+d.eval.melted <- melt(d.eval, id.vars = c("corpus", "model", "totgauss"))
 
 ########
 # PLOT ####
 ########
 pdf(file="figures/plot_hmm_evaluations.pdf", width = 6, height = 4)
 
-ggplot(d.eval.melted, aes(x=totgauss, y=value, col=lang, pch=model)) + 
+ggplot(d.eval.melted, aes(x=totgauss, y=value, col=corpus, pch=model)) + 
   facet_wrap(~variable) +
   geom_point(size=2) + geom_line() +
-  xlim(0,15) + ylim(20, 50) + 
+  xlim(0,15) + ylim(0, 100) + 
   xlab("Number of Gaussians (x1000)") + ylab("Error rate (%)") +
+  scale_color_manual(values=c("orange", "purple4")) +
   theme_ita() + theme(legend.position="bottom")
 
 dev.off()
