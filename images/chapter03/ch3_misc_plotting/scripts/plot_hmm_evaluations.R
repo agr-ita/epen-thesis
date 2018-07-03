1c442ceedd240eeda8c6e339569338bd81ae2c09
Adriana Guevara Rukoz <sk8r_babyita@hotmail.com>
2018-07-03 05:38:14 +0200

    Ch3: Correlation analyses. Parl-lms-coart update

--- images/chapter03/ch3_misc_plotting/scripts/plot_hmm_evaluations.R
+++ images/chapter03/ch3_misc_plotting/scripts/plot_hmm_evaluations.R
@@ -35,19 +35,22 @@ theme_ita <- function (base_size = 15, base_family = "Times") {
 # CREATE DATA ####
 ###############
 
-d.eval <- data.frame(corpus=c(rep("CSJ",5), rep("KCSS",5)), 
-                     model=rep(c(rep("mono", 4), "tri"), 2),
-                     totgauss=rep(c(1, 3, 6, 15, 15), 2),
-                     WER=c(c(47.24, 42.28, 40.14, 37.64, 29.94), c(81.39, 77.2, 75.59, 74.03, 65.48)),
-                     PER=c(c(44.16, 40.92, 39.47, 38.07, 25.33), c(56.23, 53.05, 51.66, 50.70, 38.42)))
-d.eval.melted <- melt(d.eval, id.vars = c("corpus", "model", "totgauss"))
+d.eval <- data.frame(corpus=c(rep("CSJ",6), rep("KCSS",6)), 
+                     model=rep(c(rep("mono", 4), "tri", "mono"), 2),
+                     totgauss=rep(c(1, 3, 6, rep(15,3)), 2),
+                     set=rep(c(rep("valid", 5), "test"), 2),
+                     WER=c(c(47.24, 42.28, 40.14, 37.64, 29.94, 37.48), 
+                           c(81.39, 77.2, 75.59, 74.03, 65.48, 73.35)),
+                     PER=c(c(44.16, 40.92, 39.47, 38.07, 25.33, 37.96), 
+                           c(56.23, 53.05, 51.66, 50.70, 38.42, 50.7)))
+d.eval.melted <- melt(d.eval, id.vars = c("corpus", "model", "totgauss", "set"))
 
 ########
 # PLOT ####
 ########
 pdf(file="figures/plot_hmm_evaluations.pdf", width = 6, height = 4)
 
-ggplot(d.eval.melted, aes(x=totgauss, y=value, col=corpus, pch=model)) + 
+ggplot(d.eval.melted[d.eval.melted$set=="valid",], aes(x=totgauss, y=value, col=corpus, pch=model)) + 
   facet_wrap(~variable) +
   geom_point(size=2) + geom_line() +
   xlim(0,15) + ylim(0, 100) + 
