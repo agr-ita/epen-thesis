61b654b1221e2724a3267bc822e7b919822a9a26
Adriana Guevara Rukoz <sk8r_babyita@hotmail.com>
2018-07-07 03:17:32 +0200

    Ch3: Eval modifs

--- images/chapter03/ch3_misc_plotting/scripts/plot_hmm_evaluations.R
+++ images/chapter03/ch3_misc_plotting/scripts/plot_hmm_evaluations.R
@@ -35,14 +35,16 @@ theme_ita <- function (base_size = 15, base_family = "Times") {
 # CREATE DATA ####
 ###############
 
-d.eval <- data.frame(corpus=c(rep("CSJ",6), rep("KCSS",6)), 
-                     model=rep(c(rep("mono", 4), "tri", "mono"), 2),
-                     totgauss=rep(c(1, 3, 6, rep(15,3)), 2),
-                     set=rep(c(rep("valid", 5), "test"), 2),
+d.eval <- data.frame(corpus=c(rep("CSJ",6), rep("KCSS",6), rep("WSJ",6)), 
+                     model=rep(c(rep("mono", 4), "tri", "mono"), 3),
+                     totgauss=rep(c(1, 3, 6, rep(15,3)), 3),
+                     set=rep(c(rep("valid", 5), "test"), 3),
                      WER=c(c(47.24, 42.28, 40.14, 37.64, 29.94, 37.48), 
-                           c(81.39, 77.2, 75.59, 74.03, 65.48, 73.35)),
+                           c(81.39, 77.2, 75.59, 74.03, 65.48, 73.35),
+                           c(18.54, 14.55, 12.81, 11.26, 8.48, 11.5)), #11.44 test wpdT
                      PER=c(c(44.16, 40.92, 39.47, 38.07, 25.33, 37.96), 
-                           c(56.23, 53.05, 51.66, 50.70, 38.42, 50.7)))
+                           c(56.23, 53.05, 51.66, 50.70, 38.42, 50.7),
+                           c(NA, NA, NA, NA, NA, NA)))
 d.eval.melted <- melt(d.eval, id.vars = c("corpus", "model", "totgauss", "set"))
 
 ########
@@ -50,12 +52,13 @@ d.eval.melted <- melt(d.eval, id.vars = c("corpus", "model", "totgauss", "set"))
 ########
 pdf(file="figures/plot_hmm_evaluations.pdf", width = 6, height = 4)
 
-ggplot(d.eval.melted[d.eval.melted$set=="valid",], aes(x=totgauss, y=value, col=corpus, pch=model)) + 
+ggplot(d.eval.melted[d.eval.melted$set=="valid",], aes(x=totgauss, y=value, col=corpus, pch=model, label=(round(value, 1)))) + 
   facet_wrap(~variable) +
   geom_point(size=2) + geom_line() +
+  # geom_text(vjust = -1.5, check_overlap = F) + 
   xlim(0,15) + ylim(0, 100) + 
   xlab("Number of Gaussians (x1000)") + ylab("Error rate (%)") +
-  scale_color_manual(values=c("orange", "purple4")) +
+  scale_color_manual(values=c("orange", "black", "purple4")) +
   theme_ita() + theme(legend.position="bottom")
 
 dev.off()
